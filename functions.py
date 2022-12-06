a474f9e76cae7c03adec220bc8c975142c4285b8
Morris Chan <morrismanfung@gmail.com>
2022-12-05 17:18:18 -0800

    Tackling translation problem.

--- functions.py
+++ functions.py
@@ -1,6 +1,8 @@
+import numpy as np
 import altair as alt
 alt.renderers.enable('mimetype')
 alt.data_transformers.enable('data_server')
+
 
 def hist( train_df, feat = None, feat_list = None, repeat = False, title = '', n_bins = 20):
     if repeat == False:
@@ -19,7 +21,7 @@ def hist( train_df, feat = None, feat_list = None, repeat = False, title = '', n
         chart_list_concat = []
         for feat in feat_list:
             chart_tmp_0 = alt.Chart( train_df.query('default==0')).mark_bar().encode(
-                alt.X( feat, type='quantitative', bin = alt.Bin( maxbins = n_bins), scale = alt.Scale( domain = ( train_df[ feat].min()-1, train_df[ feat].max()+1))),
+                alt.X( feat, type='quantitative', bin = alt.Bin( maxbins = n_bins), scale = alt.Scale( domain = ( min( train_df[ feat].min()-1, 0), train_df[ feat].max()+1))),
                 alt.Y( 'count()', stack=False, title=''),
                 alt.Color( 'default', type='ordinal', scale=alt.Scale(scheme='category10'))
             ).properties(
@@ -27,7 +29,7 @@ def hist( train_df, feat = None, feat_list = None, repeat = False, title = '', n
                 width=300
             )
             chart_tmp_1 = alt.Chart( train_df.query('default==1')).mark_bar().encode(
-                alt.X( feat, type='quantitative', bin = alt.Bin( maxbins = n_bins), scale = alt.Scale( domain = ( train_df[ feat].min()-1, train_df[ feat].max()+1))),
+                alt.X( feat, type='quantitative', bin = alt.Bin( maxbins = n_bins), scale = alt.Scale( domain = ( min( train_df[ feat].min()-1, 0), train_df[ feat].max()+1))),
                 alt.Y( 'count()', stack=False, title=''),
                 alt.Color( 'default', type='ordinal', scale=alt.Scale(scheme='category10'))
             ).properties(
@@ -79,3 +81,9 @@ def bar( train_df, feat = None, feat_list = None, repeat = False, title = ''):
             chart_concat = chart_tmp_0 | chart_tmp_1
             chart_list_concat.append( chart_concat)
         return alt.vconcat( *chart_list_concat, title = title)
+
+def log_func(x):
+    return np.log(x+1)
+
+def translation_funct( x):
+    return x - min( x) + 1
